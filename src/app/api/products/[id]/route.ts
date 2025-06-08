4784a29542909f073b9209821c01676f885b2e67
Sachin Pal <sachinpal9352@gmial.com>
2025-06-08 13:14:04 +0530

    nerel

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,23 +1,22 @@
-import { NextRequest, NextResponse } from "next/server";
-import connectDB from "@/lib/mongodb";
+import connectMongoDB  from "@/lib/mongodb";
 import { Product } from "@/models/Product";
+import { NextResponse } from "next/server";
 
-// 👇 No need to define custom type; Next.js automatically provides `params`
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
-  await connectDB();
+export async function GET(_: Request, { params }: { params: { id: string } }) {
+  await connectMongoDB();
   const product = await Product.findById(params.id);
   return NextResponse.json(product);
 }
 
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
-  await connectDB();
+export async function PUT(req: Request, { params }: { params: { id: string } }) {
+  await connectMongoDB;
   const body = await req.json();
   const updated = await Product.findByIdAndUpdate(params.id, body, { new: true });
   return NextResponse.json(updated);
 }
 
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
-  await connectDB();
+export async function DELETE(_: Request, { params }: { params: { id: string } }) {
+  await connectMongoDB();
   await Product.findByIdAndDelete(params.id);
   return NextResponse.json({ success: true });
 }
--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,15 +1,15 @@
-import connectDB from "@/lib/mongodb";
+import  connectMongoDB  from "@/lib/mongodb";
 import { Product } from "@/models/Product";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  await connectDB();
+  await connectMongoDB();
   const products = await Product.find();
   return NextResponse.json(products);
 }
 
 export async function POST(req: Request) {
-  await connectDB();
+  await connectMongoDB;
   const body = await req.json();
   const newProduct = await Product.create(body);
   return NextResponse.json(newProduct);
