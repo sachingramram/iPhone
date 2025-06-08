767faf65af01a704d37072a9eda245292a9150fa
Sachin Pal <sachinpal9352@gmial.com>
2025-06-08 15:43:44 +0530

    nedcsddfesfdsfsdfsd

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,21 +1,21 @@
-import connectMongoDB  from "@/lib/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+import connectMongoDB from "@/lib/mongodb";
 import { Product } from "@/models/Product";
-import { NextResponse } from "next/server";
 
-export async function GET(_: Request, { params }: { params: { id: string } }) {
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
   await connectMongoDB();
   const product = await Product.findById(params.id);
   return NextResponse.json(product);
 }
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
-  await connectMongoDB;
-  const body = await req.json();
+export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
+  await connectMongoDB();
+  const body = await request.json();
   const updated = await Product.findByIdAndUpdate(params.id, body, { new: true });
   return NextResponse.json(updated);
 }
 
-export async function DELETE(_: Request, { params }: { params: { id: string } }) {
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
   await connectMongoDB();
   await Product.findByIdAndDelete(params.id);
   return NextResponse.json({ success: true });
